9fe865a1e496ba8c5dc754f353ce81d9d278dde3
src_resources <src.res.211@gmail.com>
2025-05-15 10:20:33 +0800

    Make NEMU quit gracefully

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -17,6 +17,7 @@
 #include <cpu/cpu.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <utils.h>
 #include "sdb.h"
 
 static int is_batch_mode = false;
@@ -49,6 +50,7 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
+  nemu_state.state = NEMU_QUIT;
   return -1;
 }
 
