6cdc2bac51b8dbbe58001d97fe3457cda13615d7
src_resources <src.res.211@gmail.com>
2025-10-14 22:42:59 +0800

    npc: add loader for SRAM

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <klib-macros.h>
 #include <riscv/riscv.h>
 
@@ -20,7 +21,29 @@ void halt(int code) {
     while (1);
 }
 
+extern char _sram_start, _data_start, _data_end;
+
+static void load_data_section(void) {
+    volatile const uint8_t *src, *src_end;
+    volatile uint8_t *dst;
+    uint8_t val;
+
+    for (
+        src = (const uint8_t *) (intptr_t) _data_start,
+        src_end = (const uint8_t *) (intptr_t) _data_end,
+        dst = (uint8_t *) (intptr_t) _sram_start;
+        src < src_end;
+        src++, dst++
+    ) {
+        val = *src;
+        *dst = val;
+    }
+}
+
 void _trm_init(void) {
-    int ret = main(mainargs);
+    int ret;
+
+    load_data_section();
+    ret = main(mainargs);
     halt(ret);
 }
