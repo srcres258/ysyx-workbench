feba84d9e7772a6301b88ec07186878ed39fdd00
src_resources <src.res.211@gmail.com>
2025-12-06 13:00:21 +0800

    Update flake

--- flake.nix
+++ flake.nix
@@ -21,6 +21,15 @@
     ];
     runtimeDeps = with pkgs; [
       SDL2
+      SDL2_image
+      SDL2_ttf
+      sdl3
+      sdl3-ttf
+      sdl3-image
+
+      libelf
+      libz
+      capstone
     ];
   in {
     devShells.default = my-system.devShells.${system}.srcres-full.overrideAttrs (old: {
@@ -29,13 +38,23 @@
 
       hardeningDisable = [ "all" ];
 
+      packages = with pkgs; [
+        verilator
+      ];
+
       shellHook = ''
         export PKG_CONFIG_PATH="${pkgs.lib.makeSearchPath "lib/pkgconfig" runtimeDeps}"
         export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeDeps}:$LD_LIBRARY_PATH"
 
+        #export CPATH="${pkgs.verilator}/share/verilator/include:$CPATH"
+        #export LIBRARY_PATH="${pkgs.verilator}/lib:$LIBRARY_PATH"
+
         export NVBOARD_HOME="${builtins.getEnv "PWD"}/nvboard"
         export AM_HOME="${builtins.getEnv "PWD"}/abstract-machine"
         export NPC_HOME="${builtins.getEnv "PWD"}/npc"
+        export VERILATOR_HOME="${pkgs.verilator}/share/verilator"
+        export NEMU_HOME="${builtins.getEnv "PWD"}/nemu"
+        export YSYX_HOME="${builtins.getEnv "PWD"}"
       '' + (old.shellHook or "");
     });
   });
