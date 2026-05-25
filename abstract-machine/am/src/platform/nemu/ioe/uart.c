9875f00c2266635f77466671b73f4dd2fe8b56ef
srcres258 <src.res.211@gmail.com>
2026-05-25 11:28:45 +0800

    Implement UART input feature for NEMU
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- abstract-machine/am/src/platform/nemu/ioe/uart.c
+++ abstract-machine/am/src/platform/nemu/ioe/uart.c
@@ -6,9 +6,14 @@ void __am_uart_config(AM_UART_CONFIG_T *cfg) {
 }
 
 void __am_uart_tx(AM_UART_TX_T *uart) {
+    while (!(inb(SERIAL_PORT + 5) & 0x20));
     outb(SERIAL_PORT, uart->data);
 }
 
 void __am_uart_rx(AM_UART_RX_T *uart) {
-    uart->data = (char)-1;
+    if (inb(SERIAL_PORT + 5) & 0x01) {
+        uart->data = inb(SERIAL_PORT);
+    } else {
+        uart->data = (char)-1;
+    }
 }
--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -15,26 +15,56 @@
 
 #include <utils.h>
 #include <device/map.h>
+#include <sys/select.h>
+#include <unistd.h>
 
 /* http://en.wikibooks.org/wiki/Serial_Programming/8250_UART_Programming */
 // NOTE: this is compatible to 16550
 
 #define CH_OFFSET 0
+#define LSR_OFFSET 5
+#define LSR_RX_READY 0x01
+#define LSR_TX_EMPTY 0x20
 
 static uint8_t *serial_base = NULL;
-
+static int rx_char = -1;
 
 static void serial_putc(char ch) {
   MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
 }
 
+static int try_getchar() {
+  fd_set fds;
+  FD_ZERO(&fds);
+  FD_SET(0, &fds);
+  struct timeval tv = {0, 0};
+  if (select(1, &fds, NULL, NULL, &tv) > 0) {
+    char ch;
+    if (read(0, &ch, 1) == 1) return (uint8_t)ch;
+  }
+  return -1;
+}
+
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   assert(len == 1);
   switch (offset) {
-    /* We bind the serial port with the host stderr in NEMU. */
     case CH_OFFSET:
-      if (is_write) serial_putc(serial_base[0]);
-      else panic("do not support read");
+      if (is_write) {
+        serial_putc(serial_base[0]);
+      } else {
+        if (rx_char >= 0) {
+          serial_base[0] = (uint8_t)rx_char;
+          rx_char = -1;
+        }
+      }
+      break;
+    case LSR_OFFSET:
+      if (!is_write) {
+        if (rx_char < 0) rx_char = try_getchar();
+        uint8_t lsr = LSR_TX_EMPTY;
+        if (rx_char >= 0) lsr |= LSR_RX_READY;
+        serial_base[LSR_OFFSET] = lsr;
+      }
       break;
     default: panic("do not support offset = %d", offset);
   }
